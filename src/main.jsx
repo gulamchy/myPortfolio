2609152420c4fb14d97a802b45099ed5fdfcf11a
gulamchy <gulamsulaman@gmail.com>
2024-10-26 04:01:37 -0700

    Fixing SmoothScroll name

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import SmoothScrolling from './components/lenis/SmoothScrolling.jsx'
+import SmoothScrolling from './components/lenis/smoothScrolling.animation.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <SmoothScrolling>
       <App />
     </SmoothScrolling>
+
   </StrictMode>,
 )
